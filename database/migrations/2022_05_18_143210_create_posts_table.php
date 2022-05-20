95fe8be083526d34a68319f7379023881ddb19e9
hongorgerelchimeg <g.hongoroo@gmail.com>
2022-05-20 17:00:04 +0200

    database setup + creation

--- database/migrations/2022_05_18_143210_create_posts_table.php
+++ database/migrations/2022_05_18_143210_create_posts_table.php
@@ -19,6 +19,12 @@ class CreatePostsTable extends Migration
             $table->text('content');
             $table->string('slug', 100)->unique();
             $table->timestamps();
+
+            $table->unsignedBigInteger('user_id');
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
         });
     }
 
